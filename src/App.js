2e663feaeb3c68af0eb0705be8ea77b7ef94a234
iser-80 <issamerrafiy01@gmail.com>
2023-11-17 07:53:45 +0100

    add the event slice api

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Main from './pages/main/main';
 import Login from './pages/login/login'
 import Register from './pages/register/register'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ArtistPage from './pages/artist/artist';
+import EventPage from './pages/event/eventPage'
 
 function App() {
   return (
@@ -11,6 +13,10 @@ function App() {
         <Route path='/' element={<Main />} />
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
+
+        // to modidy later
+        <Route path='/event' element={<EventPage />} />
+        <Route path='/artist' element={<ArtistPage />} />
       </Routes>
     </Router>
   );
---
+++ src/redux_toolkit/slices/api/eventApiSlice.js
@@ -0,0 +1,71 @@
+const { createSlice, createAsyncThunk } = require("@reduxjs/toolkit");
+
+export const organizationAddEventAsync = createAsyncThunk('event/addEvent', async (data) => {
+    const response = await fetch('http://locahost:5000/api/organization/addEvent', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        credentials: 'include',
+        headers: { 'Content-Type': 'application/json' }
+    })
+
+    const result = response.json()
+    return result
+})
+
+export const organizationDeleteEventAsync = createAsyncThunk('event/deleteEvent', async (eventId) => {
+    const response = await fetch('http://locahost:5000/api/organization/deleteEvent', {
+        method: 'DELETE',
+        body: JSON.stringify({eventId}),
+        credentials: 'include',
+        headers: { 'Content-Type': 'application/json' }
+    })
+    const result = response.json()
+    return result
+})
+
+
+// this for chosing the artists in the event
+
+export const eventAddArtistAsync = createAsyncThunk('event/addArtist', async (data) => {
+    const response = await fetch('http://localhost:5000/api/event/addArtist', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        credentials: 'include',
+        headers: { 'Content-Type': 'application/json' }
+    })
+    const result = response.json()
+    return result
+})
+
+export const eventDeleteArtistAsync = createAsyncThunk('event/deleteArtist', async ({eventId, artistId}) => {
+    const response = await fetch('http://localhost:5000/api/event/deleteArtist', {
+        method: 'DELETE',
+        body: JSON.stringify({eventId, artistId}),
+        credentials: 'include',
+        headers: { 'Content-Type': 'application/json' }
+    })
+    const result = response.json()
+    return result
+})
+
+
+const eventSlice = createSlice({
+    name: 'event',
+    initialState: {
+        events: []
+    },
+    reducers: [],
+    extraReducers: (builder) => {
+        builder
+            .addCase(organizationAddEventAsync.fulfilled, (state, action) => {
+                state.events.push(action.payload)
+            })
+            .addCase(organizationDeleteEventAsync.fulfilled, (state, action) => {
+                const eventId = action.payload.eventId
+                state.events = state.events.filter((event) => event._id !== eventId)
+            })
+    }
+})
+
+
+export default eventSlice.reducer
