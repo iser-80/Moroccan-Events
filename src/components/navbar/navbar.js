96bab8ae2a0c7a7fa67cbf46927e2d1791f2bbb2
iser-80 <issamerrafiy01@gmail.com>
2023-11-23 12:30:32 +0100

    fix the navbar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,28 +1,36 @@
 import React from 'react'
 import styles from './navbar.module.css'
 import { Link } from 'react-scroll'
+import { Router, Link as RouterLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <nav>
         <div className={styles.navbarContainer}>
             <div className={styles.navbarLogo}>
-                events
+                FESTI-FLOW
             </div>
             <ul>
                 <li>
                     <Link to="home" spy={true} smooth={true} duration={500} className={styles.navbarLink} >Home</Link>
                 </li>
                 <li>
-                    <Link to="home" spy={true} smooth={true} duration={500} className={styles.navbarLink}  >Home</Link>
+                    <Link to="events" spy={true} smooth={true} duration={500} className={styles.navbarLink}  >Events</Link>
                 </li>
                 <li>
-                    <Link to="home" spy={true} smooth={true} duration={500} className={styles.navbarLink}  >Home</Link>
+                    <Link to="artists" spy={true} smooth={true} duration={500} className={styles.navbarLink}  >Artists</Link>
                 </li>
                 <li>
-                    <Link to="home" spy={true} smooth={true} duration={500} className={styles.navbarLink}  >Home</Link>
+                    <Link to="about" spy={true} smooth={true} duration={500} className={styles.navbarLink}  >About</Link>
+                </li>
+                <li>
+                    <Link to="sponsor" spy={true} smooth={true} duration={500} className={styles.navbarLink}  >Sponsors</Link>
                 </li>
             </ul>
+            <div className={styles.authentificationBtns}>
+                <RouterLink to='/login' className={styles.authentificationBtn}>Login</RouterLink>
+                <RouterLink to='/register' className={styles.authentificationBtn}>Register</RouterLink>
+            </div>
         </div>
     </nav>
   )
--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -6,17 +6,31 @@ import About from '../../sections/about/about'
 import Sponsor from '../../sections/news/Sponsor'
 import Events from '../../sections/events/events'
 import styles from './main.module.css'
+import { Element } from 'react-scroll'
 
 const Main = () => {
   return (
     <div className={styles.mainContainer}>
-      
       <Navbar/>
-      <Hero/> 
-         <Events />
-        <Artists/>
-        <About />
-        <Sponsor />
+        <Element name='home'>
+          <Hero/>
+        </Element> 
+
+        <Element name='events'>
+          <Events />
+        </Element>
+
+        <Element name='artists'>
+          <Artists/>
+        </Element>
+
+        <Element name='about'>
+          <About />
+        </Element>
+
+        <Element name='sponsor'>
+          <Sponsor />
+        </Element>
     </div>
   )
 }
